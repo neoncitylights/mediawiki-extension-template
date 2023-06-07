f15a4f50f6186a8df72ad1836249a8f1da7d2d35
Samantha <sam.t.nguyenn@gmail.com>
2023-06-06 23:34:04 -0500

    ci: add initial CI workflow (#1)

--- includes/Specials/SpecialHelloWorld.php
+++ includes/Specials/SpecialHelloWorld.php
@@ -8,10 +8,16 @@ use SpecialPage;
  * @license MIT
  */
 class SpecialHelloWorld extends SpecialPage {
+	/**
+	 * @inheritDoc
+	 */
 	public function __construct() {
 		parent::__construct( 'HelloWorld' );
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function execute( $sub ) {
 		$this->setHeaders();
 		$this->outputHeader();
